4f18b60715af7c5db7a3e5c8335893ceb3d3f365
shazaraza <44866791+shazaraza@users.noreply.github.com>
2019-08-06 23:37:33 +0100

    Add files via upload

--- delete.js
+++ delete.js
@@ -1,27 +1,58 @@
-// add event listener to the iframe object
+// we are utlising the cookies to store the next nextVide
+// regex, if the string is toonova then find the element of the char array that
+//it is stored in.
 let sourcesClass = document.getElementsByClassName("vmargin");
 var sourceNumber = sourcesClass.length;
 var sourceArray=[];
 try{
   var nextVideo = document.getElementById("elinks").children[1];
+  var prevVideo = document.getElementById("elinks").children[0];
 }
 catch(err){
   console.log(err.message);
 }
-var i;
-for (i = 0; i < sourceNumber; i++) {
-  if(sourcesClass[i].children[1].firstElementChild.src!= null){
-  sourceArray.push(sourcesClass[i].children[1].firstElementChild.src)
+try{
+DifferentVideo(nextVideo,9);
+DifferentVideo(prevVideo,16);
 }
-  //add video type to all sources
+catch(err){
+}
+var savedVideo = document.cookie
+document.addEventListener("keydown",saveVideo);
+function saveVideo(e){
+  if(e.keyCode == 48){
+    if (confirm("Would you like to save video?")) {
+      document.cookie = window.location.href
+    }
+    else {
+      //alert("No video saved");
+    }
+  }
+}
+var chars = savedVideo.split(';'); // everything works up to the for loop
+ // we do need to iterate through the array
+var i;
+
+for (i = 0; i < chars.length; i++) {
+  if (chars[i].includes('toonova')){
+    var correctSave = chars[i];
+  }
+}
+document.addEventListener("keydown",loadVideo);
+function loadVideo(e){
+  if(e.keyCode == 189){
+    window.open(correctSave,"_self");
+  }
 }
 
-sourceArray.forEach(function(element) {
-  element.addEventListener("ended",function(){
-      if(nextVideo!= null){
-        if(nextVideo.href!=null){
-        window.open(nextVideo.href,"_self");
-      }
-    }
-});
-});
+
+function DifferentVideo(video,keyNumber){
+  document.addEventListener("keydown",function(e){
+  if (e.keyCode === keyNumber){
+    if(video!= null&&video.href!=null){
+      window.open(video.href,"_self");
+
+  }
+}
+  });
+}
