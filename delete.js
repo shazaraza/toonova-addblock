e4b8c9314992ef2d7b71b08ce98771cd7bacc02b
shazaraza <44866791+shazaraza@users.noreply.github.com>
2020-02-06 11:08:40 +0000

    Add files via upload

--- delete.js
+++ delete.js
@@ -1,8 +1,11 @@
-// add event listener to the iframe object
-// replace link if value is null
+// we are utlising the cookies to store the next nextVide
+// regex, if the string is toonova then find the element of the char array that
+//it is stored in.
 let sourcesClass = document.getElementsByClassName("vmargin");
 var sourceNumber = sourcesClass.length;
 var sourceArray=[];
+
+// getting the links to the previous and the next videos
 try{
   var nextVideo = document.getElementById("elinks").children[1];
   var prevVideo = document.getElementById("elinks").children[0];
@@ -10,20 +13,62 @@ try{
 catch(err){
   console.log(err.message);
 }
+// this section removes the annoying header from the website
+// this deletes the side margin ads, if dont exist caught by error
 try{
-DifferentVideo(nextVideo,9);
-DifferentVideo(prevVideo,16);
+  var blockSideMarginAds = document.getElementById("body").children[1].children[0];
+  blockSideMarginAds.remove()
+  let blockedMarginParent = document.getElementById("upper-header").children[0].children[0].children[2].children[0];
+  blockedMarginParent.remove()
+}
+catch(err){}
+
+try{
+DifferentVideo(nextVideo,9); // this gets the next video in the playlist
+DifferentVideo(prevVideo,16); // this gets the previous video in the playlist
 }
 catch(err){
-  console.log(err.message);
 }
-function DifferentVideo(video,keyNumber){
-  document.addEventListener("keydown",function(e){
-    if (e.keyCode === keyNumber){
-      if(video!= null&&video.href!=null){
-        window.open(video.href,"_self");
 
+var savedVideo = document.cookie
+document.addEventListener("keydown",saveVideo);// allows me to save an episode in cookies
+
+function saveVideo(e){
+  if(e.keyCode == 48){
+    if (confirm("Would you like to save video?")) {
+      document.cookie = window.location.href
+    }
+    else {
+      alert("No video saved");
     }
   }
+}
+var chars = savedVideo.split(';'); // everything works up to the for loop
+ // we do need to iterate through the array
+var i;
+
+// this is saying that if i find an element in the cookies that has the word Toonova
+// then i know that it is my saved video
+for (i = 0; i < chars.length; i++) {
+  if (chars[i].includes('toonova')){
+    var correctSave = chars[i];
+  }
+}
+document.addEventListener("keydown",loadVideo);
+function loadVideo(e){
+  if(e.keyCode == 189){
+    window.open(correctSave,"_self");
+  }
+}
+
+
+function DifferentVideo(video,keyNumber){
+  document.addEventListener("keydown",function(e){
+  if (e.keyCode === keyNumber){
+    if(video!= null&&video.href!=null){
+      window.open(video.href,"_self");
+
+  }
+}
   });
 }
